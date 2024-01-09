566ce3655144035b0c6004b6565c778d12112c1d
StarlightJS <nolanium@proton.me>
2024-01-09 16:56:41 +0000

    Initial commit

---
+++ website_example/config.js
@@ -0,0 +1,16 @@
+var parentCoin = "Nolanium";
+
+var api = "https://pool.nolanium.xyz:8118";
+var poolHost = "pool.nolanium.xyz";
+
+var email = "";
+var telegram = "";
+var discord = "https://discord.gg/YKh5GjTGmU";
+
+var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
+
+var blockchainExplorer = "https://explorer.nolanium.xyz/block?hash={id}";
+var transactionExplorer = "https://explorer.nolanium.xyz/transaction?hash={id}";
+
+var themeCss = "themes/default.css";
+var defaultLang = "en";
